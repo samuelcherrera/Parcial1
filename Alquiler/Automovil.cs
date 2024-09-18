310cfd9a9c559912aab6891126f844121c44e913
AndersonCoronado357 <andersoncoronado157@gmail.com>
2024-09-17 23:13:37 -0500

    Commit 1 Anderson

---
+++ Alquiler/Automovil.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alquiler
+{
+    public class Automovil : Vehiculo, IGestionReservas
+    {
+        public int Kilometraje { get; set; }
+
+        public Automovil(string Marca, string Modelo, int AñoDeFabricacion, Estado Estado, int Kilometraje)
+            : base(Marca, Modelo, AñoDeFabricacion, Estado)
+        {
+            this.Kilometraje = Kilometraje;
+        }
+
+        public override double CalcularPrecioAlquiler(int dias)
+        {
+            return 65.59 * dias;
+        }
+
+        public void reservar()
+        {
+            if (Estado == Estado.DISPONIBLE)
+            {
+                Estado = Estado.ALQUILADO;
+            }
+            else
+            {
+                Console.WriteLine("El automóvil no está disponible para ser alquilado.");
+            }
+        }
+
+        public void devolver()
+        {
+            if (Estado == Estado.ALQUILADO)
+            {
+                Estado = Estado.DISPONIBLE;
+            }
+            else
+            {
+                Console.WriteLine("El automóvil no se puede devolver.");
+            }
+        }
+    }
+}
