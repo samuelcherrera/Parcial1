446b14000ba0721c1d4e0350203879d367303435
samuelcherrera <samuelcasta597gmail.com>
2024-09-17 23:16:26 -0500

    commit 2 samuel

---
+++ Alquiler/Camion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alquiler
+{
+    public class Camion : Vehiculo, IGestionReservas
+    {
+        public int CapacidadDeCarga { get; set; }
+
+        public Camion(string Marca, string Modelo, int AñoDeFabricacion, Estado Estado, int CapacidadDeCarga)
+            : base(Marca, Modelo, AñoDeFabricacion, Estado)
+        {
+            this.CapacidadDeCarga = CapacidadDeCarga;
+        }
+
+        public override double CalcularPrecioAlquiler(int dias)
+        {
+            return 80.00 * dias;
+        }
+
+        public void reservar()
+        {
+            if (Estado == Estado.DISPONIBLE)
+            {
+                Estado = Estado.ALQUILADO;
+            }
+            else
+            {
+                Console.WriteLine("El camión no está disponible para ser alquilado.");
+            }
+        }
+
+        public void devolver()
+        {
+            if (Estado == Estado.ALQUILADO)
+            {
+                Estado = Estado.DISPONIBLE;
+            }
+            else
+            {
+                Console.WriteLine("El camión no se puede devolver.");
+            }
+        }
+    }
+}
