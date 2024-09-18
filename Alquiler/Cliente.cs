3d8db7d784bbcea76ac1ec1f56932df4e7707a2f
AndersonCoronado357 <andersoncoronado157@gmail.com>
2024-09-17 23:21:16 -0500

    Commit 2 Anderson

---
+++ Alquiler/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alquiler
+{
+    public class Cliente
+    {
+        public string? Name { get; private set; }
+        public int Id { get; private set; }
+
+        public Cliente(string Name, int Id)
+        {
+            this.Name = Name;
+            this.Id = Id;
+        }
+    }
+}
---
+++ Alquiler/Motocicleta.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alquiler
+{
+    public class Motocicleta : Vehiculo, IGestionReservas
+    {
+        public int Cilindraje { get; set; }
+
+        public Motocicleta(string Marca, string Modelo, int AñoDeFabricacion, Estado Estado, int Cilindraje)
+            : base(Marca, Modelo, AñoDeFabricacion, Estado)
+        {
+            this.Cilindraje = Cilindraje;
+        }
+
+        public override double CalcularPrecioAlquiler(int dias)
+        {
+            return 47.50 * dias;
+        }
+
+        public void reservar()
+        {
+            if (Estado == Estado.DISPONIBLE)
+            {
+                Estado = Estado.ALQUILADO;
+            }
+            else
+            {
+                Console.WriteLine("La motocicleta no está disponible para ser alquilada.");
+            }
+        }
+
+        public void devolver()
+        {
+            if (Estado == Estado.ALQUILADO)
+            {
+                Estado = Estado.DISPONIBLE;
+            }
+            else
+            {
+                Console.WriteLine("La motocicleta no se puede devolver.");
+            }
+        }
+    }
+}
